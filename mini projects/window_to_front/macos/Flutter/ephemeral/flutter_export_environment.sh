671baea47d4abf6d0d439004893cf2e8b85c04f7
Akshit Batra <akbatra567@gmail.com>
2020-09-28 21:35:42 +0530

    MacOs Flutter Desktop Github Application completed

---
+++ mini projects/window_to_front/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/akshitbatra/flutter120/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/akshitbatra/projects/#100DaysOfCode/mini projects/window_to_front"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/akshitbatra/flutter120/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
